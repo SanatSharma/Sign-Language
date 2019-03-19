b5e4b42b252d65f9793014852a02079948dae7b1
Sanat Sharma <sanatcolumban@gmail.com>
2019-03-19 00:20:35 -0500

    Small changes

--- load_images.py
+++ load_images.py
@@ -10,12 +10,14 @@ def pickle_images_labels():
 	images = []
 	labels = []
 	for g_id in os.listdir(gest_folder):
-		for i in range(2400):
-			print("LOADING", gest_folder+"/"+g_id+"/"+str(i+1)+".jpg")
+		print("Loading", gest_folder+"/"+g_id)
+		for i in range(1000):
+			#print("LOADING", gest_folder+"/"+g_id+"/"+str(i+1)+".jpg")
 			img = cv2.imread(gest_folder+"/"+g_id+"/"+str(i+1)+".jpg", 0)
 			if np.any(img == None):
 				continue
 			images_labels.append((np.array(img, dtype=np.uint8), int(g_id)))
+
 	return images_labels
 
 def split_images_labels(images_labels):
@@ -31,25 +33,27 @@ images_labels = shuffle(shuffle(shuffle(shuffle(images_labels))))
 images, labels = split_images_labels(images_labels)
 print("Length of images_labels", len(images_labels))
 
-train_images = images[:int(5/6*len(images))]
+train_ratio = .8
+
+train_images = images[:int(train_ratio*len(images))]
 print("Length of train_images", len(train_images))
 with open("train_images", "wb") as f:
 	pickle.dump(train_images, f)
 del train_images
 
-train_labels = labels[:int(5/6*len(labels))]
+train_labels = labels[:int(train_ratio*len(labels))]
 print("Length of train_labels", len(train_labels))
 with open("train_labels", "wb") as f:
 	pickle.dump(train_labels, f)
 del train_labels
 
-test_images = images[int(5/6*len(images)):]
+test_images = images[int(train_ratio*len(images)):]
 print("Length of test_images", len(test_images))
 with open("test_images", "wb") as f:
 	pickle.dump(test_images, f)
 del test_images
 
-test_labels = labels[int(5/6*len(labels)):]
+test_labels = labels[int(train_ratio*len(labels)):]
 print("Length of test_labels", len(test_labels))
 with open("test_labels", "wb") as f:
 	pickle.dump(test_labels, f)
